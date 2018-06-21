1f73e273e6f9c3e70d231a0f679278f9e4045315
Anastasios <tlalos@hotmail.com>
2018-06-21 15:54:58 +0300

    20-06-2018

--- app/src/main/java/com/example/tlalos/myapplication/FuncHelper.java
+++ app/src/main/java/com/example/tlalos/myapplication/FuncHelper.java
@@ -23,6 +23,7 @@ public class FuncHelper {
 
 
     public static String ENDPOINT_ADDRESS = "http://192.168.1.47:45455";
+    //public static String ENDPOINT_ADDRESS = "http://191.101.23.235/webapi";
     public static String ENDPOINT_GETDATA = ENDPOINT_ADDRESS+"/api/data/getmobiledata";
     public static String ENDPOINT_POST_EXPENSES_DATA = ENDPOINT_ADDRESS+"/api/data/PostExpensesData";
     public static String ENDPOINT_POST_EXPENSETYPE_DATA = ENDPOINT_ADDRESS+"/api/data/PostExpenseTypeData";
